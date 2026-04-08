d3206759acd44af00cbdd4314aee0ad63cc9152f
konayeong <dnjfrhkd402@naver.com>
2026-04-08 17:47:00 +0900

    Step 7-2

--- src/main/java/com/fbp/engine/runner/demo/step6/Step6_4.java
+++ src/main/java/com/fbp/engine/runner/demo/step6/Step6_4.java
@@ -1,84 +1,72 @@
 package com.fbp.engine.runner.demo.step6;
 
-import com.fbp.engine.core.Connection;
-import com.fbp.engine.message.Message;
 import com.fbp.engine.node.PrintNode;
 import com.fbp.engine.node.TimerNode;
 import com.fbp.engine.node.utils.SplitNode;
+import com.fbp.engine.runner.Flow;
 
+// Step 7-3 재구성
 public class Step6_4 {
     public static void run() {
-        TimerNode timerNode = new TimerNode("timer", 500);
-        SplitNode splitNode = new SplitNode("filter", "tick", 3);
-        PrintNode normalPrintNode = new PrintNode("normal");
-        PrintNode warnPrintNode = new PrintNode("warn");
+        Flow flow = new Flow("flow test 2");
 
-        Connection conn1 = new Connection("conn1");
-        Connection conn2 = new Connection("conn2");
-        Connection conn3 = new Connection("conn3");
+        flow.addNode(new TimerNode("timer", 500))
+            .addNode(new SplitNode("split", "tick", 3))
+            .addNode(new PrintNode("normal"))
+            .addNode(new PrintNode("warn"));
 
-        // connect
-        conn1.setTarget(splitNode.getInputPort("in"));
-        conn2.setTarget(normalPrintNode.getInputPort("in"));
-        conn3.setTarget(warnPrintNode.getInputPort("in"));
+        flow.connect("timer", "out", "split", "in")
+            .connect("split", "match", "print", "in")
+            .connect("split", "mismatch", "print", "in");
 
-        timerNode.getOutputPort("out").connect(conn1);
-        splitNode.getOutputPort("match").connect(conn2);
-        splitNode.getOutputPort("mismatch").connect(conn3);
+        flow.initialize();
 
-        timerNode.initialize();
-        splitNode.initialize();
-        normalPrintNode.initialize();
-        warnPrintNode.initialize();
-
-        Thread split = new Thread(() -> {
-            while(!Thread.currentThread().isInterrupted()) {
-                Message message = conn1.poll();
-                if(message == null) break;
-                splitNode.process(message);
-            }
-        });
-
-        Thread normalPrint = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                Message message = conn2.poll();
-                if(message == null) break;
-                normalPrintNode.process(message);
-            }
-        });
-
-        Thread warnPrint = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                Message message = conn3.poll();
-                if(message == null) break;
-                warnPrintNode.process(message);
-            }
-        });
-
-        split.start();
-        normalPrint.start();
-        warnPrint.start();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        timerNode.shutdown();
-        splitNode.shutdown();
-        normalPrintNode.shutdown();
-
-        split.interrupt();
-        normalPrint.interrupt();
-        warnPrint.interrupt();
-
-        try {
-            split.join();
-            normalPrint.join();
-            warnPrint.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        Thread split = new Thread(() -> {
+//            while(!Thread.currentThread().isInterrupted()) {
+//                Message message = conn1.poll();
+//                if(message == null) break;
+//                splitNode.process(message);
+//            }
+//        });
+//
+//        Thread normalPrint = new Thread(() -> {
+//            while (!Thread.currentThread().isInterrupted()) {
+//                Message message = conn2.poll();
+//                if(message == null) break;
+//                normalPrintNode.process(message);
+//            }
+//        });
+//
+//        Thread warnPrint = new Thread(() -> {
+//            while (!Thread.currentThread().isInterrupted()) {
+//                Message message = conn3.poll();
+//                if(message == null) break;
+//                warnPrintNode.process(message);
+//            }
+//        });
+//
+//        split.start();
+//        normalPrint.start();
+//        warnPrint.start();
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        flow.shutdown();
+//
+//        split.interrupt();
+//        normalPrint.interrupt();
+//        warnPrint.interrupt();
+//
+//        try {
+//            split.join();
+//            normalPrint.join();
+//            warnPrint.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
--- src/main/java/com/fbp/engine/runner/demo/step6/Step6_5.java
+++ src/main/java/com/fbp/engine/runner/demo/step6/Step6_5.java
@@ -1,79 +1,66 @@
 package com.fbp.engine.runner.demo.step6;
 
-import com.fbp.engine.core.Connection;
-import com.fbp.engine.message.Message;
-import com.fbp.engine.node.FilterNode;
-import com.fbp.engine.node.LogNode;
-import com.fbp.engine.node.PrintNode;
-import com.fbp.engine.node.TimerNode;
-
+import com.fbp.engine.node.*;
+import com.fbp.engine.runner.Flow;
+// Step 7-2 재구성
 public class Step6_5 {
     public static void run() throws InterruptedException {
-        TimerNode timerNode = new TimerNode("timer", 500);
-        LogNode logNode = new LogNode("log");
-        FilterNode filterNode = new FilterNode("filter", "tick", 3);
-        PrintNode printNode = new PrintNode("print");
+        Flow flow = new Flow("[Step7-2] Flow Refactoring");
 
-        Connection timerLog = new Connection("timerLog");
-        Connection logFilter = new Connection("logFilter");
-        Connection filterPrint = new Connection("filterPrint");
+        // 노드 등록
+        flow.addNode(new TimerNode("timer", 500))
+                .addNode(new LogNode("log"))
+                .addNode(new FilterNode("filter", "tick", 3))
+                .addNode(new PrintNode("print"));
 
-        // connect
-        timerLog.setTarget(logNode.getInputPort("in"));
-        logFilter.setTarget(filterNode.getInputPort("in"));
-        filterPrint.setTarget(printNode.getInputPort("in"));
+        // 연결 정의
+        flow.connect("timer", "out", "log", "in")
+            .connect("log", "out", "filter", "in")
+            .connect("filter", "out", "print", "in");
 
-        timerNode.getOutputPort("out").connect(timerLog);
-        logNode.getOutputPort("out").connect(logFilter);
-        filterNode.getOutputPort("out").connect(filterPrint);
 
         // initialize
-        timerNode.initialize();
-        logNode.initialize();
-        filterNode.initialize();
-        printNode.initialize();
+        flow.initialize();
 
-        Thread t1 = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                Message message = timerLog.poll();
-                if(message == null) break;
-                logNode.process(message);
-            }
-        });
-
-        Thread t2 = new Thread(() -> {
-            while(!Thread.currentThread().isInterrupted()) {
-                Message message = logFilter.poll();
-                if(message == null) break;
-                filterNode.process(message);
-            }
-        });
-
-        Thread t3 = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                Message message = filterPrint.poll();
-                if (message == null) break;
-                printNode.process(message);
-            }
-        });
-
-        t1.start();
-        t2.start();
-        t3.start();
-
-        Thread.sleep(7000);
-
-        timerNode.shutdown();
-        logNode.shutdown();
-        filterNode.shutdown();
-        printNode.shutdown();
-
-        t1.interrupt();
-        t2.interrupt();
-        t3.interrupt();
-
-        t1.join();
-        t2.join();
-        t3.join();
+        // TODO ?
+//        Thread t1 = new Thread(() -> {
+//            while (!Thread.currentThread().isInterrupted()) {
+//                Message message = timerLog.poll();
+//                if(message == null) break;
+//                logNode.process(message);
+//            }
+//        });
+//
+//        Thread t2 = new Thread(() -> {
+//            while(!Thread.currentThread().isInterrupted()) {
+//                Message message = logFilter.poll();
+//                if(message == null) break;
+//                filterNode.process(message);
+//            }
+//        });
+//
+//        Thread t3 = new Thread(() -> {
+//            while (!Thread.currentThread().isInterrupted()) {
+//                Message message = filterPrint.poll();
+//                if (message == null) break;
+//                printNode.process(message);
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//
+//        Thread.sleep(7000);
+//
+//        flow.shutdown();
+//
+//        t1.interrupt();
+//        t2.interrupt();
+//        t3.interrupt();
+//
+//        t1.join();
+//        t2.join();
+//        t3.join();
     }
 }
